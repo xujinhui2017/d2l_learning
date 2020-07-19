aa228d87b7dcbf7fd5cfbf7df7e8a9c0e58ef16d
xujinhui <jinhui_xu@berkeley.edu>
2020-07-19 16:52:09 +0800

    update

--- script/matrix_factorization.py
+++ script/matrix_factorization.py
@@ -21,52 +21,89 @@ class MatrixFactorization(torch.nn.Module):
 
 
 def read_original_data(filename: str):
-    user_list = []
-    item_list = []
-    matrix_dict = dict()
+    train_dict = dict()
+    test_dict = dict()
+    train_list = []
+    test_list = []
+    max_user_id, min_user_id, max_item_id, min_item_id = [10] * 4
+
     with open(filename, "r", encoding="utf-8") as txt_file:
         for idx, line in enumerate(txt_file):
-            if idx > 1000:
+            if idx > 500:
                 break
-            user_id, item_id, rating = line.strip().split('\t')[:3]
+            user_id, item_id, rating, times = line.strip().split('\t')
             user_id = int(user_id)
             item_id = int(item_id)
             rating = int(rating)
-            user_list.append(user_id)
-            item_list.append(item_id)
-            matrix_dict[str(user_id) + "_" + str(item_id)] = rating
-    return user_list, item_list, matrix_dict
+
+            if user_id > max_user_id:
+                max_user_id = user_id
+            elif user_id < min_user_id:
+                min_user_id = user_id
+
+            if item_id > max_item_id:
+                max_item_id = item_id
+            elif item_id < min_item_id:
+                min_item_id = item_id
+
+            train_dict.setdefault(user_id, []).append((item_id, rating))
+            if user_id not in test_dict or times > test_dict[user_id][-1]:
+                test_dict[user_id] = (item_id, rating, times)
+
+    for user_id in train_dict:
+        for info in train_dict[user_id]:
+            item_id = info[0]
+            if item_id != test_dict[user_id][0]:
+                train_list.append((user_id, item_id, info[1]))
+            else:
+                test_list.append((user_id, item_id, info[1]))
+    return train_list, test_list, [min_user_id, max_user_id], [min_item_id, max_item_id]
+
+
+def write_format(target_list: list):
+    return "\t".join([str(i) for i in target_list]) + "\n"
+
 
 if __name__ == "__main__":
-    user_ids, item_ids, matrix_info = read_original_data(filename="data/u.data")
-    model = MatrixFactorization(n_items=max(item_ids) + 1, n_users=max(user_ids) + 1, n_factors=300)
+    train_data, test_data, max_min_user, max_min_item = read_original_data(filename="data/u.data")
+
+    # train
+    model = MatrixFactorization(n_items=max_min_item[1], n_users=max_min_user[1], n_factors=300)
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    epochs = 5
+    optimizer = torch.optim.SparseAdam(model.parameters(), lr=0.05)
+    epochs = 50
     for epoch in range(epochs):
         print(epoch)
         loss = 0
-        loss0 = 0
-        for r, c in zip(user_ids, item_ids):
-            i = torch.LongTensor([r])
-            j = torch.LongTensor([c])
-            if str(r) + "_" + str(c) not in matrix_info:
-                continue
-            rating = torch.FloatTensor([matrix_info[str(r) + "_" + str(c)]])
+        for element in train_data:
+            user_id, item_id, rating = element
+            i = torch.LongTensor([user_id - 1])
+            j = torch.LongTensor([item_id - 1])
+            rating = torch.FloatTensor([rating])
             # predict
             prediction = model.forward(i, j)
             loss += loss_fn(prediction, rating)
-            loss0 += loss_fn(prediction, rating)
-            for params in model.parameters():
-                loss += 0.01 * torch.norm(params, 1)
-                loss += 0.01 * torch.norm(params, 2)
-
+            # for params in model.parameters():
+            #     loss += 0.01 * torch.norm(params, 1)
+            #     loss += 0.01 * torch.norm(params, 2)
         # Reset the gradients to 0
         optimizer.zero_grad()
-
         # backpropagate
         loss.backward()
-
         # update weights
         optimizer.step()
-        print(loss0)
+        print(loss)
+
+    # test
+    mse = 0
+    result_file = open("data/mf.test_result", "w", encoding="utf-8")
+    for element in test_data:
+        user_id, item_id, rating = element
+        user_id_function = torch.LongTensor([user_id - 1])
+        item_id_function = torch.LongTensor([item_id - 1])
+        predict_value = (model.users_vectors(user_id_function) * model.items_vectors(item_id_function)).sum(-1) + model.users_bias(user_id_function).sum(-1) + model.items_bias(item_id_function).sum(-1)
+        predict_value = predict_value.detach().numpy().reshape(-1).tolist()[0]
+        mse += (rating - predict_value) ** 2
+        result_file.write(write_format(target_list=[user_id, item_id, rating, predict_value]))
+    result_file.close()
+    print(mse)
